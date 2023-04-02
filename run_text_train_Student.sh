ff382f716db2f81466a0ec30e4052b0aa6e605c1
hoangpnhat <hoangpnhat@mail.com>
2023-04-02 19:30:42 +0700

    Add attentive module, class TextRes5ROIHead, TextRes5ROIHeads_VKV

---
+++ run_text_train_Student.sh
@@ -0,0 +1,42 @@
+# run file: bash bash/danhnt.sh ablations 1
+EXP_NAME=$1
+SPLIT_ID=$2
+
+# N_GPUS=8
+# export CUDA_VISIBLE_DEVICES=4,5,6,7,0,1,2,3
+N_GPUS=1
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
+# export CUDA_VISIBLE_DEVICES=4,5,6,7
+
+
+IMAGENET_PRETRAIN=ImageNetPretrained/MSRA/R-101.pkl
+IMAGENET_PRETRAIN_TORCH=ImageNetPretrained/torchvision/resnet101-5d3b4d8f.pth
+IMAGENET_PRETRAIN=/home/hoangpn/Ecai/DeFRCN/checkpoints/voc/singleHeadAtt_Text/teacher_base/defrcn_det_r101_base1/model_final.pth
+# train teacher model
+SAVE_DIR=checkpoints/voc/${EXP_NAME}
+TEACHER_PATH=checkpoints/voc/${EXP_NAME}/teacher_base/defrcn_det_r101_base${SPLIT_ID}
+
+TEACHER_WEIGHT=${TEACHER_PATH}/model_final.pth
+
+# train student model
+cfg_MODEL='
+MODEL.ROI_HEADS.NAME TextRes5ROIHeads
+MODEL.ROI_HEADS.TEACHER_TRAINING True
+MODEL.ROI_HEADS.STUDENT_TRAINING True
+MODEL.ROI_HEADS.DISTILLATE True
+TEST.EVAL_PERIOD 500000
+MODEL.ROI_HEADS.L2 True
+MODEL.ROI_HEADS.KL True
+'
+
+STUDENT_PATH=${SAVE_DIR}/student_base/defrcn_det_r101_base${SPLIT_ID}
+
+python3 tools/model_surgery.py --dataset voc --method randinit                                \
+   --src-path ${TEACHER_PATH}/model_final.pth                    \
+   --save-dir ${TEACHER_PATH}
+
+python3 main.py --num-gpus ${N_GPUS} --dist-url auto --config-file configs/voc/defrcn_det_r101_base${SPLIT_ID}.yaml     \
+   --opts MODEL.WEIGHTS ${TEACHER_WEIGHT}                                 \
+       OUTPUT_DIR ${STUDENT_PATH} TEST.PCB_MODELPATH ${IMAGENET_PRETRAIN_TORCH} ${cfg_MODEL}
+
+
